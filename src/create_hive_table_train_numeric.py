39e9c7fdacb4f7de15d28b86371eab6fb011fd61
Kyrylo Pogrebenko <kpogrebenko@kpogrebenk-nbk2.local>
2016-10-19 15:16:36 -0400

    .

--- src/create_hive_table_train_numeric.py
+++ src/create_hive_table_train_numeric.py
@@ -6,6 +6,11 @@
 import os
 import re
 
+step1 = False
+step2 = True
+step3 = True
+step4 = True
+
 db = 'bosch'
 table = 'train_numeric'
 table_temp = 'train_numeric_temp'
@@ -14,15 +19,22 @@ table_temp = 'train_numeric_temp'
 create_table_temp = '''
 /usr/bin/hive -e "create table {0}.{1} (col_value STRING)"
 '''.strip().format(db, table_temp)
-os.system(create_table_temp);
+print(create_table_temp)
+
+if step1:
+    os.system(create_table_temp)
 
 # Step 2. Load data into temp table
 load_data_temp = '''
 /usr/bin/hive -e "LOAD DATA INPATH '/user/hdfs/{0}/{1}.csv' OVERWRITE INTO TABLE {0}.{2}"
 '''.strip().format(db, table, table_temp)
-os.system(load_data_temp)
+print(load_data_temp)
+
+if step2:
+    os.system(load_data_temp)
 
 # Step 3. Create permanent table
+
 work_dir = os.getcwd()
 input_dir = '/'.join(work_dir.split('/')[:-1]) + '/input/'
 f = [e for e in os.listdir(input_dir) if re.match('^train_numeric\.csv$', e)].pop()
@@ -44,6 +56,10 @@ print(header)
 create_table_perm = '''
 /usr/bin/hive -e "{0}"
 '''.strip().format(header)
+print(create_table_perm)
+
+if step3:
+    os.system(create_table_perm)
 
 # Step 4. Update the table with the values taken from the temp table
 header1 = "insert overwrite table bosch.train_numeric select "
@@ -58,9 +74,11 @@ for n, c in enumerate(cols, start = 1):
         c = insertion.replace('CNT', str(n)) + ' 1) ' + c
     header1 += c
 header1 += footer1
-print(header1)
 
 update_table_perm = '''
 /usr/bin/hive -e "{0}"
 '''.strip().format(header1)
-os.system(header1)
+print(update_table_perm)
+
+if step4:
+    os.system(update_table_perm)

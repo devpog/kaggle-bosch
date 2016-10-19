bbf35cb76e2be36097854fce421bc6ba1a0fc2a6
Kyrylo Pogrebenko <kpogrebenko@kpogrebenk-nbk2.local>
2016-10-18 23:22:47 -0400

    .

--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
 import os
+import re
+
+work_dir = os.getcwd()
+input_dir = '/'.join(work_dir.split('/')[:-1]) + '/input/'
+
+train_files = [input_dir + f for f in os.listdir(input_dir) if re.match(r'^train.*\.csv$', f)]
+train_keys = [re.match(r'^train_(.*)\.csv$', f).group(1) for f in os.listdir(input_dir) if re.match(r'^train_(.*)\.csv$', f)]
+train = dict(zip(train_keys, train_files))
+
+test_files = [input_dir + f for f in os.listdir(input_dir) if re.match(r'^test.*\.csv$', f)]
+test_keys = [re.match(r'^test_(.*)\.csv$', f).group(1) for f in os.listdir(input_dir) if re.match(r'^test_(.*)\.csv$', f)]
+test = dict(zip(test_keys, test_files))
+

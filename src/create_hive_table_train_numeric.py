2c3e866f60425eedbfbc5d8bd7260b2e03515a92
Kyrylo Pogrebenko <kpogrebenko@kpogrebenk-nbk2.local>
2016-10-19 14:59:18 -0400

    .

--- src/create_hive_table_train_numeric.py
+++ src/create_hive_table_train_numeric.py
@@ -10,24 +10,23 @@ db = 'bosch'
 table = 'train_numeric'
 table_temp = 'train_numeric_temp'
 
-# Step 1. Create a temp table
-create_table_temp = """
-hive -e "create table {0}.{1} (col_value STRING)"
-""".strip().format(db, table_temp)
+# Step 1. Create temp table
+create_table_temp = '''
+/usr/bin/hive -e "create table {0}.{1} (col_value STRING)"
+'''.strip().format(db, table_temp)
 os.system(create_table_temp);
 
 # Step 2. Load data into temp table
-load_data_temp = """
-hive -e "LOAD DATA INPATH '/user/hdfs/{0}/{1}.csv' OVERWRITE INTO TABLE {0}.{2}"
-""".strip().format(db, table, table_temp)
+load_data_temp = '''
+/usr/bin/hive -e "LOAD DATA INPATH '/user/hdfs/{0}/{1}.csv' OVERWRITE INTO TABLE {0}.{2}"
+'''.strip().format(db, table, table_temp)
 os.system(load_data_temp)
 
-# Step 3.
+# Step 3. Create permanent table
 work_dir = os.getcwd()
 input_dir = '/'.join(work_dir.split('/')[:-1]) + '/input/'
 f = [e for e in os.listdir(input_dir) if re.match('^train_numeric\.csv$', e)].pop()
 h = [e for e in os.listdir(input_dir) if re.match('^train_numeric_header\.csv$', e)].pop()
-
 cols = open(input_dir + h).read().split(',')
 
 header = "create table bosch.train_numeric ("
@@ -42,6 +41,11 @@ for n, c in enumerate(cols):
     header += c
 print(header)
 
+create_table_perm = '''
+/usr/bin/hive -e "{0}"
+'''.strip().format(header)
+
+# Step 4. Update the table with the values taken from the temp table
 header1 = "insert overwrite table bosch.train_numeric select "
 insertion = """
 regexp_extract(col_value, '^(?:([^,]*),?){CNT}',
@@ -54,5 +58,9 @@ for n, c in enumerate(cols, start = 1):
         c = insertion.replace('CNT', str(n)) + ' 1) ' + c
     header1 += c
 header1 += footer1
-
 print(header1)
+
+update_table_perm = '''
+/usr/bin/hive -e "{0}"
+'''.strip().format(header1)
+os.system(header1)
